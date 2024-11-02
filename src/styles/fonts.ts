1541316d78109cfa2780aa164bec2ff3d46099ea
Cleyton Furtado <cleytonrodrigues69@live.com>
2024-11-02 11:05:44 -0300

    feat: config prettier and fonts

---
+++ src/styles/fonts.ts
@@ -0,0 +1,8 @@
+import { Spectral } from 'next/font/google';
+
+export const spectral = Spectral({
+  weight: ['700'],
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-spectral',
+});
