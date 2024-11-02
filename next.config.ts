e991cdf02c8c0b683ae87673c74252037a063eb2
Cleyton Furtado <cleytonrodrigues69@live.com>
2024-11-02 17:17:02 -0300

    feat: added remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dogsapi.origamid.dev',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
