360dff9c2a2c7801aa0a06fb3384afa7cc07c1eb
Cleyton Furtado <cleytonrodrigues69@live.com>
2025-03-18 16:14:28 -0300

    feat: added metadata to Login pages

--- src/app/login/criar/page.tsx
+++ src/app/login/criar/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Crie sua conta | Dogs',
+  description: 'Cria sua conta no site Insta Dogs.',
+};
+
 export default function Criar() {
-  return <h1>Criar</h1>;
+  return (
+    <main>
+      <h1>Criar</h1>
+    </main>
+  );
 }
--- src/app/login/perdeu/page.tsx
+++ src/app/login/perdeu/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Perdeu  a senha | Dogs',
+  description: 'Recupere a sua senha.',
+};
+
 export default function Perdeu() {
   return <h1>Perdeu</h1>;
 }
