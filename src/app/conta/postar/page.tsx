fa2ad53e582e8601311f794cd7449b41ab55f272
Cleyton Furtado <cleytonrodrigues69@live.com>
2025-03-26 11:18:04 -0300

    feat: added metadata to post photo page

--- src/app/conta/postar/page.tsx
+++ src/app/conta/postar/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Post foto | insta dogs',
+  description: 'Página para postar novas fotos',
+};
+
 export default function Postar() {
   return <h1>Postar</h1>;
 }
