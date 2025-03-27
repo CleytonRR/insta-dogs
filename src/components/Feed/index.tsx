6ea5e08e78fcc334d6ec306acb35eaf7bc15ccd5
Cleyton Furtado <cleytonrodrigues69@live.com>
2025-03-27 15:56:45 -0300

    feat: added new Feed in account and route pag

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,66 +1,18 @@
-'use client';
-
-import React, { useEffect } from 'react';
-import FeedModal from '../FeedModal';
+import React from 'react';
 import FeedPhotos from '../FeedPhotos';
+import getPhotos from '@/actions/getPhotos';
 
-type FeedProps = {
-  user?: [string, number];
-};
-
-const Feed = ({ user }: FeedProps) => {
-  const [modalPhoto, setModalPhoto] = React.useState<object | null>(null);
-  const [pages, setPages] = React.useState([1]);
-  const [infinite, setInfinite] = React.useState(true);
-
-  useEffect(() => {
-    let wait = false;
-    function infiniteScroll() {
-      if (infinite) {
-        const scroll = window.scrollY;
-        const height = document.body.offsetHeight - window.innerHeight;
-        if (scroll > height * 0.75 && !wait) {
-          setPages((pages) => [...pages, pages.length + 1]);
-          wait = true;
-          setTimeout(() => {
-            wait = false;
-          }, 500);
-        }
-      }
-    }
-
-    window.addEventListener('wheel', infiniteScroll);
-    window.addEventListener('scroll', infiniteScroll);
-    return () => {
-      window.removeEventListener('wheel', infiniteScroll);
-      window.removeEventListener('scroll', infiniteScroll);
-    };
-  }, [infinite]);
+export default async function Feed() {
+  const { data: photos } = await getPhotos(
+    {},
+    {
+      cache: 'no-store',
+    },
+  );
 
   return (
     <section>
-      {modalPhoto && (
-        <FeedModal
-          photo={modalPhoto as { id: string }}
-          setModalPhoto={setModalPhoto}
-        />
-      )}
-      {pages.map((page) => (
-        <FeedPhotos
-          key={page}
-          user={user as [string, number]}
-          page={page}
-          setModalPhoto={(e) => setModalPhoto(e as unknown as object)}
-          setInfinite={setInfinite}
-        />
-      ))}
-      {!infinite && !user && (
-        <p className="px-0 pb-0 pt-8 text-center text-[#888]">
-          Não existem mais postagens.
-        </p>
-      )}
+      <FeedPhotos photos={photos} user={0} />
     </section>
   );
-};
-
-export default Feed;
+}
--- src/components/FeedPhotos/index.tsx
+++ src/components/FeedPhotos/index.tsx
@@ -1,60 +1,86 @@
-import React from 'react';
-import useFetch from '@/hooks/useFetch';
-import { PHOTOS_GET } from '@/services/api';
-import Error from '../Helpers/Error';
-import Loading from '../Helpers/Loading';
-import FeedPhotosItem from '../FeedPhotoItem';
+'use client';
 
-type FeedPhotosProps = {
-  page: number;
-  user: [string, number];
-  setModalPhoto: (param: object) => void;
-  setInfinite: (param: boolean) => void;
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import FeedPhotosItem from '../FeedPhotoItem';
+import { Photo, PhotosGetParams } from '@/types/Forms';
+import getPhotos from '@/actions/getPhotos';
+
+type FeedPhotosProps = Pick<PhotosGetParams, 'user'> & {
+  photos?: Photo[] | null;
 };
 
-const FeedPhotos = ({
-  page,
-  user,
-  setModalPhoto,
-  setInfinite,
-}: FeedPhotosProps) => {
-  const { data, loading, error, request } = useFetch();
+const FeedPhotos = ({ photos, user }: FeedPhotosProps) => {
+  const [photosFeed, setPhotosFeed] = useState(photos);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [isInfiniteScroll, setIsInfiniteScroll] = useState(
+    photos && photos?.length < 6 ? false : true,
+  );
 
-  React.useEffect(() => {
-    async function fetchPhotos() {
-      const total = 6;
-      const { url, options } = PHOTOS_GET({ page, total, user: user as never });
-      const { response, json } = await request(url, options);
-      if (response && response.ok && json.length < total) setInfinite(false);
+  const isFetching = useRef(false);
+
+  const infiniteScroll = useCallback(() => {
+    if (isFetching.current) return;
+    isFetching.current = true;
+    setLoading(true);
+
+    setTimeout(() => {
+      setPage((page) => page + 1);
+      isFetching.current = false;
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (page === 1) return;
+
+    async function getPagePhotos(page: number) {
+      const { data: photos } = await getPhotos({
+        page,
+        total: 6,
+        user,
+      });
+
+      if (photos) {
+        setPhotosFeed((photosFeed) => [...(photosFeed || []), ...photos]);
+
+        if (photos.length < 6) {
+          setIsInfiniteScroll(false);
+        }
+      }
     }
-    fetchPhotos();
-  }, [request, user, page, setInfinite]);
 
-  if (error) return <Error error={error} />;
-  if (loading) return <Loading />;
-  if (data)
-    return (
+    getPagePhotos(page);
+  }, [page, user]);
+
+  useEffect(() => {
+    if (isInfiniteScroll) {
+      window.addEventListener('scroll', infiniteScroll);
+      window.addEventListener('wheel', infiniteScroll);
+    } else {
+      window.removeEventListener('scroll', infiniteScroll);
+      window.removeEventListener('wheel', infiniteScroll);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', infiniteScroll);
+      window.removeEventListener('wheel', infiniteScroll);
+    };
+  }, [infiniteScroll, isInfiniteScroll]);
+
+  return (
+    <section>
       <ul
-        className={`animate-animeLeft mb-4 grid grid-cols-2 justify-items-center gap-[1rem] md:grid-cols-3`}
+        className={`mb-4 grid animate-animeLeft grid-cols-2 justify-items-center gap-[1rem] md:grid-cols-3`}
       >
-        {(
-          (data as Array<{
-            id: string;
-            src: string;
-            title: string;
-            acessos: string;
-          }>) ?? []
-        ).map((photo, index) => (
-          <FeedPhotosItem
-            key={photo.id}
-            photo={photo}
-            setModalPhoto={setModalPhoto}
-            index={index}
-          />
+        {photosFeed?.map((photo, index) => (
+          <FeedPhotosItem key={photo.id} photo={photo} index={index} />
         ))}
       </ul>
-    );
-  else return null;
+
+      {loading && <p className="text-center">Carregando...</p>}
+    </section>
+  );
 };
 
 export default FeedPhotos;
