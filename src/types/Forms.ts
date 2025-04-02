d3f311351f26c301eb07e2d184ac463a8277d7b0
Cleyton Furtado <cleytonrodrigues69@live.com>
2025-04-02 11:50:57 -0300

    feat: adjusted type

--- src/types/Forms.ts
+++ src/types/Forms.ts
@@ -35,9 +35,8 @@ export type Comment = {
 };
 
 export type PhotoDetail = {
-  photo: Photo & {
-    comments: Comment[];
-  };
+  photo: Photo;
+  comments: Comment[];
 };
 
 export type PhotosGetParams = {
